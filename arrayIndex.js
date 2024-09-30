972007728584f839e21a6b88cc6b35206ef7d23f
Ghanenddra Bhardwaj <130120469+GhanendraBhardwaj19@users.noreply.github.com>
2024-09-30 17:45:02 +0530

    Add files via upload

---
+++ arrayIndex.js
@@ -0,0 +1,96 @@
+let fruits = ["apple", "banana", 4, 1, 5];
+let numbers = [5, 2, 1, 8, 9, 4, 2];
+
+// push method
+// fruits.push(1, "guava");
+// console.log(fruits);
+
+// pop method
+// let lastFruit = fruits.pop();
+// console.log(fruits);
+// console.log(lastFruit);
+
+// shift
+// let firstFruit = fruits.shift();
+// console.log(fruits);
+// console.log(firstFruit);
+
+// unshift
+// let newLength = fruits.unshift("ram", "rahim");
+// console.log(fruits);
+// console.log(newLength);
+
+// map
+// let fruits1 = fruits.map((ele, ind) => {
+//   return ele;
+// });
+// console.log(fruits1);
+
+// filter
+// let fruits1 = fruits.filter((ele) => ele > 3);
+// console.log(fruits1);
+
+// reduce
+// let total = number.reduce((acc, ele) => {
+//   return acc + ele;
+// }, 0);
+// console.log(total);
+
+//forEach
+// number.forEach((number) => {
+//   console.log(number ** 2);
+// });
+
+// find
+// let number = numbers.find((num) => num == 5);
+// console.log(number);
+
+// includes
+// let result = numbers.includes(5) ? "yes" : "no";
+// console.log(result);
+
+// slice
+// let slicedNumbers = numbers.slice(2, 5);
+// console.log(slicedNumbers);
+
+// splice
+// numbers.splice(2, 0, 1, 2);
+// console.log(numbers);
+
+// numbers.splice(2, 1);
+// console.log(numbers);
+
+// numbers.splice(2, 1, 4);
+// console.log(numbers);
+
+// join
+// let result = numbers.join("-");
+// console.log(result);
+
+// sort
+// let result = numbers.sort();
+// console.log(result);
+
+//reverse
+// numbers.sort();
+// let result = numbers.reverse();
+// console.log(result);
+
+// indexOf
+// console.log(numbers.indexOf(2));
+
+// lastIndexOf
+// console.log(numbers.lastIndexOf(2));
+
+// toString
+// console.log(numbers.toString());
+
+// for..of
+// for (let value of numbers) {
+//   console.log(value);
+// }
+
+// for..in
+// for (let x in numbers) {
+//   console.log(numbers[x]);
+// }
---
+++ stringIndex.js
@@ -0,0 +1,57 @@
+let str = " ghanendra bhardwaj ";
+
+//charAt
+// console.log(str.charAt(2));
+
+// carCodeAt
+// console.log(str.charCodeAt());
+
+// concat
+// console.log(str.concat(" bhardwaj"));
+
+// includes
+// console.log(str.includes("g"));
+
+// indexOf
+// console.log(str.indexOf("h"));
+
+// lastIndexOf
+// console.log(str.lastIndexOf("a"));
+
+// slice
+// console.log(str.slice(1, 3));
+
+// split
+// console.log(str.split(" "));
+
+// toUpperCase
+// console.log(str.toUpperCase());
+
+// toLowerCase
+// console.log(str.toLowerCase());
+
+// trim
+// console.log(str.trim());
+
+// replace
+// console.log(str.replace("a", "b"));
+
+// replaceAll
+// console.log(str.replaceAll("a", "b"));
+
+// substring
+// console.log(str.substring(1, 3));
+// console.log(str.substring(-5, 5)); -values starts from index 0
+
+// substr
+// console.log(str.substr(1, 4));
+// console.log(str.substr(-3, 5)); substr allows negative values
+
+// startsWith
+// console.log(str.startsWith(" gh"));
+
+// endsWith
+// console.log(str.endsWith("ra "));
+
+// repeat
+// console.log(str.repeat(3));
